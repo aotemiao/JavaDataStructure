464c606ebd652c2eb0b2172aa46d4298b59f71cf
aotemiao <aotemiaox@gmail.com>
2021-02-01 21:38:27 +0800

    SortMethod

--- src/datasturcture/sort/QuickSort.java
+++ src/datasturcture/sort/QuickSort.java
@@ -1,7 +1,30 @@
 package datasturcture.sort;
 
+import static datasturcture.sort.Module.exch;
+import static datasturcture.sort.Module.less;
+
 public class QuickSort {
     public static void sort(Comparable[] a) {
-        
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j, hi);
+    }
+
+    public static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        return j;
     }
 }
