c84247727783f4d262293a49a6f793bee7efd7a5
aotemiao <aotemiaox@gmail.com>
2021-03-18 21:15:05 +0800

    BST

--- src/datasturcture/search/BST.java
+++ src/datasturcture/search/BST.java
@@ -3,16 +3,14 @@ package datasturcture.search;
 
 import com.sun.jdi.Value;
 
-import java.security.Key;
-
 /**
  * @author aotemiao
  * @date 2021/3/16 20:45
  */
-public class BST<key extends Comparable<key>> {
+public class BST<Key extends Comparable<Key>, Value> {
     private Node root;
 
-    private class Node<T> {//todo:内部类？
+    private class Node {//todo:内部类？
         private Key key;
         private Value val;
         private Node left, right;
@@ -23,6 +21,89 @@ public class BST<key extends Comparable<key>> {
             this.val = val;
             this.N = N;
         }
+    }
+
+    public int size() {
+        return (size(root));
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.N;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.val;
+    }
+
+    public void put(Key key, Value val) {
+        put(root, key, val);
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        x.N = size(x.left) + size(x.right) + 1;//todo:优化？
+        return x;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        return min(x.left);
+    }
+
+    public Key floor(Key key) {//小于等于key的最大键
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp < 0) return floor(x.left, key);//在右子树中寻找比key小的键
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        else return x;
+    }
+
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    private Node select(Node x, int k) {//返回排名为k的结点
+        if (x == null) return null;
+        int t = size(x.left);
+        if (t > k) return select(x.left, k);
+        else if (t < k) return select(x.right, k - t - 1);
+        else return x;
+    }
+
+    public int rank(Key key) {
+        return rank(root, key);
+    }
+    private int rank(Node x, Key key) {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return rank(x.left, key);
+        else if (cmp > 0) return 1 + size(x.left) + rank(x.right, key);
+        else return size(x.left);
     }
 
 }
